d7192a2fb82949320588abd7250469165855d3db
Harshvardhan Kadam <harshvardhankadam89@gmail.com>
2023-04-19 22:37:53 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav style={{backgroundColor:"#1D283C"}} className="navbar navbar-dark fixed-top flex-md-nowrap p-0 shadow">
+      <nav style={{backgroundColor:"#1D283C"}} className="navbar navbar-dark fixed-top  flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="https://github.com/TheHarshKadam/Decentragram"
@@ -15,7 +15,22 @@ class Navbar extends Component {
         >
           <img src={photo}  height="30" style={{margin:"10px"}} className="d-inline-block align-top" alt="" />
         </a>
-        
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+            { this.props.account
+              ? <img
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+              />
+              : <span></span>
+            }
+          </li>
+        </ul>
       </nav>
     );
   }
